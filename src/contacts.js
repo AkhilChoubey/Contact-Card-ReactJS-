6819667b81568c645cc05898f9d0567c901ddaf8
Akhil Kr Choubey <akhilchoubeys@gmail.com>
2021-08-02 18:57:47 +0530

    Style Changes

--- src/contacts.js
+++ src/contacts.js
@@ -1,27 +1,27 @@
 const contacts = [
   {
     id: 1,
-    name: "Beyonce",
+    name: "Akhil Choubey",
     imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
-    email: "b@beyonce.com"
+      "your_image.jpg",
+    phone: "+91 9693480133",
+    email: "akhilchoubeys@gmail.com"
   },
   {
     id: 2,
-    name: "Jack Bauer",
+    name: "Govind Gupta",
     imgURL:
-      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com"
+      "govind.jpg",
+    phone: "+91 9495949393",
+    email: "govind@gmail.com"
   },
   {
     id: 3,
-    name: "Chuck Norris",
+    name: "Akash Kumar",
     imgURL:
-      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
+      "Akash.png",
+    phone: "+91 7645455456",
+    email: "govind@.com"
   }
 ];
 
